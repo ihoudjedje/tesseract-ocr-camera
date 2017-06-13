19b5e8f26243c49a8ccd67afb41c4308613e7731
Ilyes Houdjedje <houdjedje_ilyes16@yahoo.fr>
2017-06-13 17:20:22 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Camera} from "@ionic-native/camera";
+import Tesseract from 'tesseract.js';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  imageUrl = '';
+  recognizedText: string;
 
-  constructor(public navCtrl: NavController) {
+
+
+  constructor(public navCtrl: NavController, private camera: Camera) {
 
   }
 
+  openCamera(){
+    this.camera.getPicture({
+      encodingType: this.camera.EncodingType.JPEG,
+      correctOrientation: true
+    })
+      .then( imageData => {
+        this.imageUrl = imageData;
+
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  recognizeText(image) {
+    this.recognizedText = 'en cours';
+    console.log(image);
+    Tesseract.recognize(image)
+      .progress((progress) => {
+        console.log('progress', progress);
+      })
+      .then((tesseractResult) => {
+        console.log(tesseractResult);
+        this.recognizedText = tesseractResult.text;
+
+      });
+  }
 }
